13d6f1725168c9e1e5d7160ff124af4ad37b5316
narugo1992 <narugo992@gmail.com>
2023-12-30 13:56:31 +0800

    dev(narugo): add docs for jikanv4 api

--- pyanimeinfo/myanimelist/jikanv4.py
+++ pyanimeinfo/myanimelist/jikanv4.py
@@ -14,6 +14,15 @@ _SearchOrderByTyping = Literal[
 
 
 class JikanV4Client:
+    """
+    Client for interacting with the Jikan API (v4) to retrieve anime and character information.
+
+    :param session: Optional requests session. If not provided, a new session is created.
+    :type session: Optional[requests.Session]
+    :param website: Optional Jikan API website URL. Defaults to the official Jikan v4 API.
+    :type website: Optional[str]
+    """
+
     def __init__(self, session: Optional[requests.Session] = None, website: Optional[str] = None):
         self._session = session or get_session()
         self._session.headers.update({
@@ -24,15 +33,43 @@ class JikanV4Client:
 
     @classmethod
     def _post_process_resp(cls, resp: requests.Response):
+        """
+        Helper method to process the API response.
+
+        :param resp: API response from the Jikan API.
+        :type resp: requests.Response
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         resp.raise_for_status()
         return resp.json()['data']
 
     def _raw_get(self, url, params: Mapping[str, Optional[str]] = None):
+        """
+        Perform a raw GET request to the Jikan API.
+
+        :param url: Relative URL for the API endpoint.
+        :type url: str
+        :param params: Optional parameters for the API request.
+        :type params: Mapping[str, Optional[str]]
+        :return: Raw response from the Jikan API.
+        :rtype: requests.Response
+        """
         url = urljoin(self._website, url)
         params = {key: str(value) for key, value in (params or {}).items() if value is not None}
         return self._session.get(url, params=params)
 
     def _get(self, url, params: Mapping[str, Optional[str]] = None):
+        """
+        Perform a GET request to the Jikan API and process the response.
+
+        :param url: Relative URL for the API endpoint.
+        :type url: str
+        :param params: Optional parameters for the API request.
+        :type params: Mapping[str, Optional[str]]
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._post_process_resp(self._raw_get(url, params))
 
     def search_anime(self, query: str,
@@ -44,6 +81,36 @@ class JikanV4Client:
                      sfw: bool = False, unapproved: bool = False,
                      start_date: Optional[str] = None, end_date: Optional[str] = None,
                      page: Optional[int] = None, limit: Optional[int] = None):
+        """
+        Search for anime on the Jikan API.
+
+        :param query: Search query.
+        :type query: str
+        :param type_: Type of anime (e.g., "tv", "movie").
+        :type type_: Optional[Literal["tv", "movie", "ova", "special", "ona", "music"]]
+        :param status: Anime status (e.g., "airing", "complete").
+        :type status: Optional[Literal["airing", "complete", "upcoming"]]
+        :param rating: Age rating of the anime.
+        :type rating: Optional[Literal["g", "pg", "pg13", "r17", "r", "rx"]]
+        :param order_by: Attribute to order the results by.
+        :type order_by: Optional[_SearchOrderByTyping]
+        :param sort_: Sort order ("desc" or "asc").
+        :type sort_: Optional[Literal["desc", "asc"]]
+        :param sfw: Whether to include safe-for-work content.
+        :type sfw: bool
+        :param unapproved: Whether to include unapproved content.
+        :type unapproved: bool
+        :param start_date: Start date filter for anime.
+        :type start_date: Optional[str]
+        :param end_date: End date filter for anime.
+        :type end_date: Optional[str]
+        :param page: Page number for paginated results.
+        :type page: Optional[int]
+        :param limit: Limit on the number of results per page.
+        :type limit: Optional[int]
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(
             'anime',
             {
@@ -63,18 +130,58 @@ class JikanV4Client:
         )
 
     def get_anime(self, anime_id):
+        """
+        Get information about a specific anime by ID.
+
+        :param anime_id: ID of the anime.
+        :type anime_id: int
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(f'anime/{anime_id}')
 
     def get_anime_full(self, anime_id):
+        """
+        Get full information about a specific anime by ID.
+
+        :param anime_id: ID of the anime.
+        :type anime_id: int
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(f'anime/{anime_id}/full')
 
     def get_anime_characters(self, anime_id):
+        """
+        Get information about characters in a specific anime by ID.
+
+        :param anime_id: ID of the anime.
+        :type anime_id: int
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(f'anime/{anime_id}/characters')
 
     def search_characters(self, query: str,
                           order_by: Optional[Literal["mal_id", "name", "favorites"]] = None,
                           sort_: Optional[Literal["desc", "asc"]] = None,
                           page: Optional[int] = None, limit: Optional[int] = None):
+        """
+        Search for characters on the Jikan API.
+
+        :param query: Search query.
+        :type query: str
+        :param order_by: Attribute to order the results by.
+        :type order_by: Optional[Literal["mal_id", "name", "favorites"]]
+        :param sort_: Sort order ("desc" or "asc").
+        :type sort_: Optional[Literal["desc", "asc"]]
+        :param page: Page number for paginated results.
+        :type page: Optional[int]
+        :param limit: Limit on the number of results per page.
+        :type limit: Optional[int]
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(
             'characters',
             {
@@ -87,19 +194,67 @@ class JikanV4Client:
         )
 
     def get_character(self, character_id):
+        """
+        Get information about a specific character by ID.
+
+        :param character_id: ID of the character.
+        :type character_id: int
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(f'characters/{character_id}')
 
     def get_character_full(self, character_id):
+        """
+        Get full information about a specific character by ID.
+
+        :param character_id: ID of the character.
+        :type character_id: int
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(f'characters/{character_id}/full')
 
     def get_character_related_animes(self, character_id):
+        """
+        Get information about anime related to a specific character by ID.
+
+        :param character_id: ID of the character.
+        :type character_id: int
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(f'characters/{character_id}/anime')
 
     def get_character_related_manga(self, character_id):
+        """
+        Get information about manga related to a specific character by ID.
+
+        :param character_id: ID of the character.
+        :type character_id: int
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(f'characters/{character_id}/manga')
 
     def get_character_voice_actors(self, character_id):
+        """
+        Get information about voice actors for a specific character by ID.
+
+        :param character_id: ID of the character.
+        :type character_id: int
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(f'characters/{character_id}/voices')
 
     def get_character_pictures(self, character_id):
+        """
+        Get pictures of a specific character by ID.
+
+        :param character_id: ID of the character.
+        :type character_id: int
+        :return: Processed JSON data from the API response.
+        :rtype: dict
+        """
         return self._get(f'characters/{character_id}/pictures')
