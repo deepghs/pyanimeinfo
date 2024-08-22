9002849e3fbd7aff5f431ea5ebf2175f4f72a5fc
narugo1992 <narugo992@gmail.com>
2024-08-22 12:19:00 +0800

    dev(narugo): use version v0.0.4

--- pyanimeinfo/config/meta.py
+++ pyanimeinfo/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = 'pyanimeinfo'
 
 #: Version of this project.
-__VERSION__ = '0.0.3'
+__VERSION__ = '0.0.4'
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'Python anime information grabber.'
